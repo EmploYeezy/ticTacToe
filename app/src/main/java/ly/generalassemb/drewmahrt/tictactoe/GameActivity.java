aeb05cf259c84756916e23598bbc5115685cf24f
Michael Marshall <hmikemarshall@gmail.com>
2016-04-25 20:17:35 -0700

    sets up finite 3x3 int array

--- app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
+++ app/src/main/java/ly/generalassemb/drewmahrt/tictactoe/GameActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class GameActivity extends AppCompatActivity {
 
     private TextView messagetext;
@@ -18,11 +20,17 @@ public class GameActivity extends AppCompatActivity {
     private TextView box7;
     private TextView box8;
     private TextView box9;
-    private int position;
-    private int counter;
+    //private char playTracker;
+    //private int position;
+    //private int counter;
     private int counter2;
-    private int mplayer1;
-    private int mplayer2;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    //private int mplayer1;
+    //private int mplayer2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +55,7 @@ public class GameActivity extends AppCompatActivity {
         //Log.d("GameActivity", player1);
 
         messagetext.setText(player1 + " is crosses");
+        final int [] [] playTracker = new int [3][3];
 
         box1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,9 +65,11 @@ public class GameActivity extends AppCompatActivity {
                     box1.setText("O");
                     messagetext.setText(player1 + "'s turn");
                     box1.setClickable(false);
+                    playTracker[0][0] = 1;
                 } else {
                     box1.setText("X");
                     messagetext.setText(player2 + "'s turn");
+                    playTracker[0][0] = 0;
                     box1.setClickable(false);
                 }
             }
@@ -71,10 +82,12 @@ public class GameActivity extends AppCompatActivity {
                 if (counter2 % 2 == 0) {
                     box2.setText("O");
                     messagetext.setText(player1 + "'s turn");
+                    playTracker[0][1] = 1;
                     box2.setClickable(false);
                 } else {
                     box2.setText("X");
                     messagetext.setText(player2 + "'s turn");
+                    playTracker[0][1] = 0;
                     box2.setClickable(false);
                 }
             }
@@ -87,10 +100,12 @@ public class GameActivity extends AppCompatActivity {
                 if (counter2 % 2 == 0) {
                     box3.setText("O");
                     messagetext.setText(player1 + "'s turn");
+                    playTracker[0][2] = 1;
                     box3.setClickable(false);
                 } else {
                     box3.setText("X");
                     messagetext.setText(player2 + "'s turn");
+                    playTracker[0][2] = 0;
                     box3.setClickable(false);
                 }
             }
@@ -102,11 +117,13 @@ public class GameActivity extends AppCompatActivity {
                 counter2++;
                 if (counter2 % 2 == 0) {
                     box4.setText("O");
-                    messagetext.setText(player1 +"'s turn");
+                    messagetext.setText(player1 + "'s turn");
+                    playTracker[1][0] = 1;
                     box4.setClickable(false);
                 } else {
                     box4.setText("X");
                     messagetext.setText(player2 + "'s turn");
+                    playTracker[1][0] = 0;
                     box4.setClickable(false);
                 }
             }
@@ -119,10 +136,12 @@ public class GameActivity extends AppCompatActivity {
                 if (counter2 % 2 == 0) {
                     box5.setText("O");
                     messagetext.setText(player1 + "'s turn");
+                    playTracker[1][1] = 1;
                     box5.setClickable(false);
                 } else {
                     box5.setText("X");
                     messagetext.setText(player2 + "'s turn");
+                    playTracker[1][1] = 0;
                     box5.setClickable(false);
                 }
             }
@@ -135,10 +154,12 @@ public class GameActivity extends AppCompatActivity {
                 if (counter2 % 2 == 0) {
                     box6.setText("O");
                     messagetext.setText(player1 + "'s turn");
+                    playTracker[1][2] = 1;
                     box6.setClickable(false);
                 } else {
                     box6.setText("X");
                     messagetext.setText(player2 + "'s turn");
+                    playTracker[1][2] = 0;
                     box6.setClickable(false);
                 }
             }
@@ -151,10 +172,12 @@ public class GameActivity extends AppCompatActivity {
                 if (counter2 % 2 == 0) {
                     box7.setText("O");
                     messagetext.setText(player1 + "'s turn");
+                    playTracker[2][0] = 1;
                     box7.setClickable(false);
                 } else {
                     box7.setText("X");
                     messagetext.setText(player2 + "'s turn");
+                    playTracker[2][0] = 0;
                     box7.setClickable(false);
                 }
             }
@@ -167,10 +190,12 @@ public class GameActivity extends AppCompatActivity {
                 if (counter2 % 2 == 0) {
                     box8.setText("O");
                     messagetext.setText(player1 + "'s turn");
+                    playTracker[2][1] = 1;
                     box8.setClickable(false);
                 } else {
                     box8.setText("X");
                     messagetext.setText(player2 + "'s turn");
+                    playTracker[2][1] = 0;
                     box8.setClickable(false);
                 }
             }
@@ -183,28 +208,15 @@ public class GameActivity extends AppCompatActivity {
                 if (counter2 % 2 == 0) {
                     box9.setText("O");
                     messagetext.setText(player1 + "'s turn");
+                    playTracker[2][2] = 1;
                     box9.setClickable(false);
                 } else {
                     box9.setText("X");
                     messagetext.setText(player2 + "'s turn");
+                    playTracker[2][2] = 0;
                     box9.setClickable(false);
                 }
             }
         });
-
     }
 }
-
-
-
-
-
-
-//
-//        box1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                counter++;
-//
-//            }
-//        });
